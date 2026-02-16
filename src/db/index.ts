cc56e8dbe5459264c44ea7eba79ef56675987ca0
intellimat <matnice23@gmail.com>
2026-02-16 13:29:19 +0100

    added_key_vault_secrets_retrieval

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,12 +1,54 @@
-// import "dotenv/config";
-import { drizzle } from "drizzle-orm/node-postgres";
+import { drizzle, NodePgDatabase } from "drizzle-orm/node-postgres";
+import { DefaultAzureCredential } from "@azure/identity";
+import { SecretClient } from "@azure/keyvault-secrets";
+import { Pool } from "pg";
 
-if (!process.env.APP_DATABASE_URL) {
-  throw new Error("APP_DATABASE_URL missing in your .env file!");
+let dbInstance:
+  | null
+  | (NodePgDatabase<Record<string, never>> & {
+      $client: Pool;
+    }) = null;
+
+async function initDb() {
+  if (dbInstance) return dbInstance;
+
+  let connectionString;
+
+  // LOCAL: Use .env.local
+  if (process.env.NODE_ENV === "development" || process.env.APP_DATABASE_URL) {
+    console.log("Using local .env connection string");
+    connectionString = process.env.APP_DATABASE_URL;
+
+    if (!connectionString) {
+      throw new Error("APP_DATABASE_URL missing in your .env.local file!");
+    }
+  }
+  // PRODUCTION: Use AzureKey Vault
+  else {
+    console.log("Fetching connection string from Azure key vault. ");
+    const credential = new DefaultAzureCredential();
+    const kvClient = new SecretClient(
+      process.env.KEY_VAULT_URL ||
+        "https://tasks-app-key-vault.vault.azure.net/",
+      credential,
+    );
+    const dbUrlSecret = await kvClient.getSecret("PG-CONNECTION-STRING");
+    connectionString = dbUrlSecret.value;
+
+    // Map env variables needed for authenticatoin and hashing (url, hasing key)
+    const nextAuthUrl = await kvClient.getSecret("NEXTAUTH-URL");
+    process.env.NEXTAUTH_URL = nextAuthUrl.value;
+    const nextAuthHasingSecret = await kvClient.getSecret("NEXTAUTH-SECRET");
+    process.env.NEXTAUTH_SECRET = nextAuthHasingSecret.value;
+  }
+
+  dbInstance = drizzle({
+    connection: {
+      connectionString,
+    },
+  });
+
+  return dbInstance;
 }
 
-export const db = drizzle({
-  connection: {
-    connectionString: process.env.APP_DATABASE_URL,
-  },
-});
+export const db = await initDb();
