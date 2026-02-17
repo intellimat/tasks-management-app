f196e034abb655ddb570173abcb5fbb40089df91
Matteo <38228915+intellimat@users.noreply.github.com>
2026-02-17 17:29:08 +0100

    removed_key_vault_access (#16)
    removed key vault access from next.js

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,4 @@
 import { drizzle, NodePgDatabase } from "drizzle-orm/node-postgres";
-import { DefaultAzureCredential } from "@azure/identity";
-import { SecretClient } from "@azure/keyvault-secrets";
 import { Pool } from "pg";
 
 let dbInstance:
@@ -14,32 +12,9 @@ async function initDb() {
 
   let connectionString: string | undefined;
 
-  if (process.env.NODE_ENV === "development" || process.env.APP_DATABASE_URL) {
-    console.log("Using local .env connection string");
-    connectionString = process.env.APP_DATABASE_URL;
-    if (!connectionString) {
-      throw new Error("APP_DATABASE_URL missing in your .env.local file!");
-    }
-  } else {
-    console.log("⏳ Fetching secrets from Azure Key Vault...");
-    const credential = new DefaultAzureCredential();
-    const kvClient = new SecretClient(
-      process.env.KEY_VAULT_URL ?? "myurl",
-      credential,
-    );
-
-    const [dbUrlSecret, nextAuthUrl, nextAuthHashingSecret] = await Promise.all(
-      [
-        kvClient.getSecret("PG-CONNECTION-STRING"),
-        kvClient.getSecret("NEXTAUTH-URL"),
-        kvClient.getSecret("NEXTAUTH-SECRET"),
-      ],
-    );
-
-    connectionString = dbUrlSecret.value;
-    process.env.NEXTAUTH_URL = nextAuthUrl.value;
-    process.env.NEXTAUTH_SECRET = nextAuthHashingSecret.value;
-    console.log("✅ Retrieved all needed secrets.");
+  connectionString = process.env.APP_DATABASE_URL;
+  if (!connectionString) {
+    throw new Error("APP_DATABASE_URL is missing in your environment!");
   }
 
   dbInstance = drizzle({ connection: { connectionString } });
